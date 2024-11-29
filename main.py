0700582458e69e2d1f76420cb55e968c6ad58d3b
blacksantaton <blacksantaton@gmail.com>
2024-11-29 09:52:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
-import os
 
 # Получаем токен из переменной окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
@@ -9,6 +9,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
+# Обработчики команд
 @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     await message.reply("Привет! Добро пожаловать в мою игру. Жми /tap, чтобы начать собирать монеты!")
@@ -18,4 +19,7 @@ async def tap(message: types.Message):
     await message.reply("Вы собрали 1 монету!")
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
+    # Указание порта по умолчанию (Render ожидает открытые порты)
+    port = os.getenv('PORT', 8080)
+    print(f"Listening on port {port}...")  # Это будет полезно для дебага
+    executor.start_polling(dp, skip_updates=True)
